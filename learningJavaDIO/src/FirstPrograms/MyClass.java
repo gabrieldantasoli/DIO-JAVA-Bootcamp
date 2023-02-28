75a4c650781bd569965aa92dd19c32658aac5935
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-02-28 12:58:14 -0300

    First Files

---
+++ learningJavaDIO/src/FirstPrograms/MyClass.java
@@ -0,0 +1,27 @@
+package FirstPrograms;
+
+import FirstProgram.Calculator.EuNaoSeiMatematica;
+
+public class MyClass {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	public void mat() {
+		// Tipo : string
+		String myName = "Gabriel";
+		// TIpo : Inteiro
+		int anoNascimento = 2003;
+		// Tipo : booleano
+		boolean calvo = false;
+		// Tipo : decimal
+		double aproximacaoPI = 3.14;
+	}
+	
+	
+	public static void print(String sentence) {
+		System.out.println(sentence);
+	}
+}
---
+++ learningJavaDIO/src/MyClass.java
@@ -0,0 +1,20 @@
+
+
+public class MyClass {
+	
+	public static void main(String[] args) {
+		// Tipo : string
+		String myName = "Gabriel";
+		// TIpo : Inteiro
+		int anoNascimento = 2003;
+		// Tipo : booleano
+		boolean calvo = false;
+		// Tipo : decimal
+		double aproximacaoPI = 3.14;
+	}
+	
+	
+	public static void print(String sentence) {
+		System.out.println(sentence);
+	}
+}
