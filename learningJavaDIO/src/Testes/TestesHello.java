05fc8f634dc06a085e002c91dfba0bcbd1be7cad
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-03-03 23:04:19 -0300

    Desafios de código

--- learningJavaDIO/src/Testes/TestesHello.java
+++ learningJavaDIO/src/Testes/TestesHello.java
@@ -1,14 +1,53 @@
 package Testes;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;import java.lang.reflect.Method;
 
+import org.junit.Before;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+
+//@TestMethodOrder(MethodOrderer.MethodName.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class TestesHello {
+	
+	@BeforeAll
+	static void init() {
+		System.out.println("Init");
+	}
+	
+	@BeforeEach
+	void ok() {
+		System.out.println("ok");
+	}
+	
+	@AfterEach
+	void ok2() {
+		System.out.println("ok2");
+	}
+	
+	@AfterAll
+	static void end() {
+		System.out.println("End");
+	}
 
+	@Order(2)
 	@Test
 	void test() {
 		System.out.println("Hello , World!");
+		assertEquals(5, 3+2);
+	}
+	
+	@Order(1)
+	@Test
+	void test2() {
+		System.out.println("Have a good day!");
 		assertEquals(5, 3+2);
 	}
 
--- learningJavaDIO/src/main/MainClasse/MainClasse.java
+++ learningJavaDIO/src/main/MainClasse/MainClasse.java
@@ -2,7 +2,11 @@ package main.MainClasse;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+
+import org.junit.jupiter.api.BeforeAll;
 
 import main.Calculator.EuNaoSeiMatematica;
 import main.CarComparations.Car;
@@ -23,7 +27,30 @@ import main.SetCollection.SetCollection;
 public class MainClasse {
 	
 	public static void main(String[] args) {
-		OrdenacaoEmCollections ord = new OrdenacaoEmCollections();
+		Scanner sc = new Scanner(System.in);
+		
+		String AN1,AN2,AN3;
+		
+		AN1 = sc.nextLine();
+		AN2 = sc.nextLine();
+		AN3 = sc.nextLine();
+		
+		Map animais = new HashMap<String, String>(){{
+			put("vertebrado ave carnivoro","aguia"); 
+			put("vertebrado ave onivoro","pomba");
+			put("vertebrado mamifero onivoro","homem");
+			put("vertebrado mamifero herbivoro","vaca");
+			
+			put("invertebrado inseto hematofago","pulga");
+			put("invertebrado inseto herbivoro","lagarta");
+			
+			put("invertebrado anelideo hematofago","sanguessuga");
+			put("invertebrado anelideo onivoro","minhoca");
+		}};
+
+		String animal = AN1 + " " + AN2 + " " + AN3;
+
+		System.out.println(animais.get(animal));
 	}
 	
 }
